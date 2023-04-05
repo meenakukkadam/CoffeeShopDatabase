c798f406f3403d8467bf178885f13ced282f15ad
Ryan Martinez <89316400+RyanMartinez02@users.noreply.github.com>
2023-04-05 16:27:06 -0400

    Add files via upload

---
+++ coffeeShop.sql
@@ -0,0 +1,91 @@
+CREATE TABLE products (
+	productID varchar(10) PRIMARY KEY,
+	productname varchar(60),
+	price DECIMAL(10,2),
+	productcost DECIMAL(10,2)
+)
+
+CREATE TABLE owners (
+	ownerID integer PRIMARY KEY,
+	fName varchar(30),
+	lName varchar(30),
+	ownAddress varchar(50),
+	email varchar(30),
+	DOB date,
+	phoneNumber integer
+);
+
+CREATE TABLE shop (
+	shopID integer PRIMARY KEY,
+	supplierID integer,
+	shopLocation varchar(30),
+	ownerID integer NOT NULL REFERENCES owners,
+	revenue integer
+);
+
+CREATE TABLE customer (
+	customerID char(10) PRIMARY KEY,
+	fName varchar(30),
+	lName varchar(30),
+	customerAddress varchar(50),
+	email varchar(30),
+	DOB date,
+	phoneNumber integer,
+	balance decimal(10,2)
+);
+
+CREATE TABLE inventory (
+	StID integer REFERENCES shop,
+	PrID varchar(10) REFERENCES products,
+	stock integer,
+	CONSTRAINT inventory_pk PRIMARY KEY(StID, PrID)
+);
+
+CREATE TABLE employees (
+	ssn integer PRIMARY KEY,
+	fName varchar(30),
+	lName varchar(30),
+	empAddress varchar(50),
+	email varchar(30),
+	DOB date,
+	phoneNumber integer,
+	storeID integer REFERENCES shop
+);
+
+CREATE TABLE managers (
+	managerID integer PRIMARY KEY REFERENCES employees ON DELETE CASCADE
+);
+
+CREATE TABLE cashiers (
+	empID integer PRIMARY KEY REFERENCES employees ON DELETE CASCADE,
+	manager integer REFERENCES managers
+);
+
+CREATE TABLE barista (
+	empID integer PRIMARY KEY REFERENCES employees ON DELETE CASCADE,
+	manager integer REFERENCES managers
+);
+
+CREATE TABLE orders (
+    orderID integer PRIMARY KEY,
+    customerID char(10) NOT NULL REFERENCES customer ON DELETE CASCADE,
+    storeID integer NOT NULL REFERENCES shop ON DELETE CASCADE,
+    dates DATE,
+    totalPrice DECIMAL(10,2),
+	cashierID integer REFERENCES cashiers,
+	baristaID integer NOT NULL REFERENCES barista
+);
+
+CREATE TABLE contain (
+	OrID integer REFERENCES orders,
+	PrID varchar(10) REFERENCES products,
+	quantity integer,
+	CONSTRAINT contains_pk PRIMARY KEY(OrID, PrID)
+);
+
+CREATE TABLE employs (
+	empID integer REFERENCES employees,
+	ownerID integer REFERENCES owners,
+	salary DECIMAL(10,2),
+	CONSTRAINT employs_pk PRIMARY KEY(empID, ownerID)
+);
